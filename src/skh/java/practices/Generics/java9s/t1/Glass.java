440d5af39f5594a570e87ea90b8c59f59b98cd46
Samiran Kumar Halder <samir@piit.local>
2017-07-03 18:03:08 +0600

    generic

---
+++ src/skh/java/practices/Generics/java9s/t1/Glass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skh.java.practices.Generics.java9s.t1;
+
+import skh.java.practices.Generics.java9s.*;
+
+/**
+ *
+ * @author samir
+ */
+public class Glass {
+    public Juice juice;
+}
---
+++ src/skh/java/practices/Generics/java9s/t1/Water.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skh.java.practices.Generics.java9s.t1;
+
+/**
+ *
+ * @author samir
+ */
+public class Water {
+    
+}
